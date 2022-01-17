c0c08101cd12f00daa4f0c74ae6b1b8c2a51e73f
autotntfan <jam310157@gmail.com>
2022-01-17 09:18:31 +0800

    modify activation and loss

--- activation.py
+++ activation.py
@@ -6,41 +6,91 @@ Created on Sun Dec 26 22:44:42 2021
 """
 
 import tensorflow as tf
+from .utils import get_realpart, get_imagpart
+from keras.layers import Layer
 
-class ComplexActivation:
+def modReLU(x):
+    b = tf.Variable(-4., trainable=True)
+    def return_value():
+        real = get_realpart(x)
+        imag = get_imagpart(x)
+        modulus = tf.sqrt(real**2 + imag**2)
+        phase = tf.atan2(imag, real)
+        cond = (tf.add(modulus, b) >= 0)
+        modulus = tf.where(cond, modulus, 0)
+        real = modulus*tf.cos(phase)
+        imag = modulus*tf.sin(phase)
+        return tf.concat([real, imag], axis=-1)
+    return return_value()
+
+def CReLU(x):
+    real = get_realpart(x)
+    imag = get_imagpart(x)
+    return tf.concat([tf.nn.relu(real),tf.nn.relu(imag)], axis=-1)
+
+def zReLU(x):
+    real = get_realpart(x)
+    imag = get_imagpart(x)
+    cond = (real >= 0) & (imag >= 0)
+    real = tf.where(cond, real, 0.)
+    imag = tf.where(cond, imag, 0.)
+    return tf.concat([real,imag], axis=-1)
+
+
+class AmplitudeMaxout(Layer):
     
-    def __init__(self):
-        pass
-    
-    def modReLU(self, x):
-        b = tf.Variable(-4., trainable=True)
-        def return_value():
-            real, imag = self.get_real_and_imag(x)
-            modulus = tf.sqrt(real**2 + imag**2)
-            phase = tf.atan2(imag, real)
-            cond = (tf.add(modulus, b) >= 0)
-            modulus = tf.where(cond, modulus, 0)
-            real = modulus*tf.cos(phase)
-            imag = modulus*tf.sin(phase)
-            return tf.concat([real, imag], axis=-1)
-        return return_value()
-    
-    def CReLU(self, x):
-        real, imag = self.get_real_and_imag(x)
-        return tf.concat([tf.nn.relu(real),tf.nn.relu(imag)], axis=-1)
-    
-    def zReLU(self, x):
-        real, imag = self.get_real_and_imag(x)
-        cond = (real >= 0) & (imag >= 0)
-        real = tf.where(cond, real, 0.)
-        imag = tf.where(cond, imag, 0.)
-        return tf.concat([real,imag], axis=-1)
-    
-    def get_real_and_imag(self, x):
+    def __init__(self, num_pieces, name='AMU',**kwargs):
+        super().__init__(**kwargs)
+        self.__name__ = name
+        self.num_pieces = num_pieces
+        
+
+    def call(self, x, axis=None):
         shape = x.get_shape().as_list()
-        if shape[-1]%2:
-            raise ValueError(f"nb of imaginary part isn't equal to that of real part")
+        if axis is None:
+            axis = -1
+            shape[0] = -1
+        if shape[axis]%2:
+            raise ValueError(f'nb of real/imaginary channel are inequivalent')
         num_channels = shape[-1]//2
-        real = x[:,:,:,num_channels:]
-        imag = x[:,:,:,:num_channels]
-        return real, imag
+        self.num_units = num_channels//self.num_pieces
+        if num_channels%self.num_pieces:
+            self.num_units += 1
+            num_padding = self.num_pieces - num_channels%self.num_pieces
+            padding_size = tf.concat([tf.shape(x)[:-1],tf.constant([num_padding])],axis=-1)
+            zero_padding = tf.zeros(padding_size)        
+        shape[axis] = self.num_units
+        exp_shape = shape + [self.num_pieces]
+        real_part = x[:,:,:,:num_channels]
+        imag_part = x[:,:,:,num_channels:]    
+        if num_channels%self.num_pieces:
+            real_part = tf.concat([real_part,zero_padding], axis=-1)
+            imag_part = tf.concat([imag_part,zero_padding], axis=-1)
+        real_part = tf.reshape(real_part, exp_shape)
+        imag_part = tf.reshape(real_part, exp_shape)
+        real_part, imag_part = self.return_AMU(real_part, imag_part, exp_shape)
+        tf.print(tf.concat([real_part,imag_part],axis=-1))
+        return tf.concat([real_part,imag_part],axis=-1)         
+
+    def compute_output_shape(self, input_shape):
+        shape = list(input_shape)
+        shape[-1] = 2*self.num_units
+        return tuple(shape)
+    
+    def get_config(self):
+        base_config = super().get_config()
+        config = {'units': self.num_units,
+                  'pieces': self.num_pieces}
+        return dict(list(base_config.items()) + list(config.items()))
+    
+    def return_AMU(self, real_part, imag_part ,expand_shape):
+
+        modulus = real_part**2 + imag_part**2
+        expand_modulus = tf.reshape(modulus, expand_shape)    
+        cond = tf.equal(expand_modulus,tf.reduce_max(expand_modulus,axis=-1,keepdims=True))
+        real_part = tf.reduce_max(real_part*tf.cast(cond,dtype=tf.float32),axis=-1)
+        imag_part = tf.reduce_max(imag_part*tf.cast(cond,dtype=tf.float32),axis=-1)    
+        return real_part, imag_part
+
+    
+
--- loss.py
+++ loss.py
@@ -6,47 +6,43 @@ Created on Sun Dec 26 22:25:07 2021
 """
 
 import tensorflow as tf
+from .utils import get_realpart, get_imagpart
 
-class ComplexLoss:
-    
-    def __init__(self):
-        pass
-        
 
-    def ComplexRMS(self, y_true, y_pred):
-        assert tf.shape(y_true) == tf.shape(y_pred)
-        n = self.feature_size(y_pred)
-        return tf.sqrt(tf.reduce_sum(self.get_square_error(y_true, y_pred)) /n)
-    
+def ComplexRMS(y_true, y_pred):
+    y_true, y_pred = _precheck(y_true, y_pred)
+    n = _feature_size(y_pred)
+    return tf.sqrt(tf.reduce_sum(_get_square_error(y_true, y_pred)) /n)
 
-    def ComplexMSE(self, y_true, y_pred):
-        assert tf.shape(y_true) == tf.shape(y_pred)
-        n = self.feature_size(y_pred) 
-        return tf.reduce_sum(self.get_square_error(y_true, y_pred)) / n
-    
 
-    def ComplexMAE(self, y_true, y_pred):
-        assert tf.shape(y_true) == tf.shape(y_pred)
-        n = self.feature_size(y_pred)
-        return tf.reduce_sum(tf.sqrt(self.get_square_error(y_true, y_pred))) / n
-    
-    def get_real_and_imag(self, x):
-        shape = x.get_shape().as_list()
-        if shape[-1]%2:
-            raise ValueError(f"nb of imaginary part isn't equal to that of real part")
-        num_channels = shape[-1]//2
-        real = x[:,:,:,num_channels:]
-        imag = x[:,:,:,:num_channels]
-        return real, imag
-    
-    def feature_size(self, x):
-        shape = x.get_shape().as_list()
-        n = 1
-        for dim in shape[1:]:
-            n = n * dim
-        return tf.cast(n,dtype=tf.float32)/2
-    
-    def get_square_error(self, y_true, y_pred):
-        real_pdt, imag_pdt = self.get_real_and_imag(y_pred)
-        real_true, imag_true = self.get_real_and_imag(y_true)
-        return (real_pdt-real_true)**2 + (imag_pdt-imag_true)**2
+def ComplexMSE(y_true, y_pred):
+    y_true, y_pred = _precheck(y_true, y_pred)
+    n = _feature_size(y_pred) 
+    return tf.reduce_sum(_get_square_error(y_true, y_pred)) / n
+
+
+def ComplexMAE(y_true, y_pred):
+    y_true, y_pred = _precheck(y_true, y_pred)
+    n = _feature_size(y_pred)
+    return tf.reduce_sum(tf.sqrt(_get_square_error(y_true, y_pred))) / n
+
+
+def _feature_size(x):
+    # to avoid the first unknown batch size, cannot use reshape(x,[-1]) to determine the nb of elements
+    shape = x.get_shape().as_list()
+    n = 1
+    for dim in shape[1:]:
+        n = n * dim
+    return tf.cast(n,dtype=tf.float32)/2
+
+def _get_square_error(y_true, y_pred):
+    print('ture',tf.shape(y_true),'pred',tf.shape(y_pred))
+    real_pdt = get_realpart(y_pred)
+    imag_pdt = get_imagpart(y_pred)
+    real_true = get_realpart(y_true)
+    imag_true = get_imagpart(y_true)
+    return (real_pdt-real_true)**2 + (imag_pdt-imag_true)**2
+
+
+def _precheck(x,y):
+    return tf.cast(x,dtype=tf.float32), tf.cast(y,dtype=tf.float32)
