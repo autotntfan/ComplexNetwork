fcedf93a947f74dc6a7b721f74389dafab024e5e
autotntfan <jam310157@gmail.com>
2022-03-21 11:02:49 +0800

    add metrics
    modify loss and act
    add MSE and SSIM metrics

--- train.py
+++ train.py
@@ -10,15 +10,17 @@ from dataset import DataPreprocessing
 from model import Model
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import numpy as np
 
 '''
     This file is main script running model and plotting outputs.
 '''
 
 
-LOAD_MODEL         = True    # whether load pretrained model, True or False
-FORWARD            = True    # whether use forward UNet or not, True or False
+LOAD_MODEL         = False    # whether load pretrained model, True or False
+FORWARD            = True   # whether use forward UNet or not, True or False
 CALLBACK           = False   # whether add callbacks in model fit, True or False
+COMPLEX            = True    # whether use complex- or real-valued network, True or False
 
 TRAINING_NUM       = 1400    # number of training data, max:1600
 FILTERS            = 16      # number of filters for the shallowest conv2D layer
@@ -30,8 +32,8 @@ VALIDATION_SPLIT   = 0.2     # ratio of validation data referred to training dat
 NFIG               = 3       # show the n-th fig of speckle, target, or prediction
 DR                 = 40      # dynamic range in dB
 
-ACTIVATION         = 'cLeakyReLU'   # Hidden-layer activation function, it could be 'modeReLU', 'cReLU', ... etc
-LOSS               = 'cMSE'         # loss function,it could be 'cMSE', 'cMAE', 'cRMS'
+ACTIVATION         = 'LeakyReLU'   # Hidden-layer activation function, it could be 'modeReLU', 'cReLU', ... etc
+LOSS               = 'ComplexMSE'         # loss function,it could be 'cMSE', 'cMAE', 'cRMS'
 
 # check and allow gpu memory to grow
 set_env()
@@ -48,14 +50,16 @@ else:
 
 
 if LOAD_MODEL:
+
     # custom model needs to add custom function
     custom_object = get_custom_object()
     if FORWARD:
-        model = tf.keras.models.load_model('modelF_bnT_100.h5',custom_objects=custom_object)
+        model = tf.keras.models.load_model('modelF_bnT_50_valid.h5',custom_objects=custom_object)
         prediction = model.predict([x_test, ideal_test])
     else:
-        model = tf.keras.models.load_model('model_bnT_200.h5',custom_objects=custom_object)
+        model = tf.keras.models.load_model('model_bnT_100_valid.h5',custom_objects=custom_object)
         prediction = model.predict(x_test)
+
 else:
     if CALLBACK:
         if VALIDATION_SPLIT:
@@ -73,13 +77,15 @@ else:
                  validation_split=VALIDATION_SPLIT,
                  activations=ACTIVATION,
                  losses=LOSS,
-                 callbacks=callbacks)
+                 callbacks=callbacks,
+                 complex_network=COMPLEX)
     if FORWARD:
         model, history = UNet([x_train, ideal_train], y_train)
         prediction = model.predict([x_test, ideal_test])
     else:
         model, history = UNet(x_train, y_train)
         prediction = model.predict(x_test)
+    np.save(f'history_epochs{EPOCHS}_foward{FORWARD}.npy',history.history)
     plt.figure()
     plt.plot(history.history['loss'], label='training loss')
     if VALIDATION_SPLIT:
@@ -87,6 +93,7 @@ else:
         plt.legend()
     plt.xlabel('epochs')
     plt.ylabel('loss')
+    plt.savefig(f'loss_epochs{EPOCHS}_foward{FORWARD}.png')
     plt.show()
       
 
@@ -99,5 +106,9 @@ show_fig(x_test[NFIG], 'speckle (dB)', DR)
 show_fig(prediction[NFIG], 'predicted image (dB)', DR)
 show_fig(y_test[NFIG], 'psf (dB)', DR)
 
+# metrics
+mse = tf.keras.losses.MeanSquaredError()
+print("MSE", mse(prediction[NFIG],y_test[NFIG]).numpy())
+print("SSIM", tf.image.ssim(prediction[NFIG],y_test[NFIG],max_val=2).numpy())
 
 tf.keras.backend.clear_session()
