afc2bd036d40d285b4ebfd4d2ea73c67eed0546a
autotntfan <jam310157@gmail.com>
2022-04-20 01:19:10 +0800

    Update train_utils.py
    show fig only handle one image, modify assertion
    envelope_detection is more robust, support ndim=4 and 3
    name of save model/fig/parameters are synced with the Model module
    image axis has been accurate
    optimize inference time

--- train_utils.py
+++ train_utils.py
@@ -23,19 +23,33 @@ def check_data_range(x):
     min_ = np.min(x)
     print(f'the largest value is {max_} and the smallest one is {min_}')
     
-def show_fig(img, ind, title_=None, DR=None):
-    axis = get_axis(img, ind)
-    r'./simulation_straight'
+def show_fig(img, ind=None, title_=None, DR=None):
+    # check image dimension is 3 or 4
+    if len(img.shape) == 4:
+        if img.shape[0] != 1:
+            raise ValueError('unable to support multiple images')
+        shape = img.shape[1:-1]
+    elif len(img.shape) == 3:
+        shape = img.shape[:-1]
+    else:
+        raise ValueError('invalid image shape')
+    if ind is None:
+        axis = None
+    else:
+        axis = _get_axis(img, ind)
     plt.figure(dpi=300)
-    if DR:
-        plt.imshow(envelope_detection(img, DR),
+    envelope_img = envelope_detection(img, DR)
+    envelope_img = envelope_img.reshape(shape)
+    assert len(envelope_img.shape) == 2
+    if DR is not None:
+        plt.imshow(envelope_img,
                    cmap='gray',
                    vmin=0,vmax=DR,
                    extent=axis,
                    aspect='auto')
         plt.colorbar()
     else:
-        plt.imshow(envelope_detection(img), cmap='gray',extent=axis,aspect='auto')
+        plt.imshow(envelope_img, cmap='gray',extent=axis,aspect='auto')
     if title_ is not None:
         assert isinstance(title_, str)
         plt.title(title_)
@@ -44,27 +58,37 @@ def show_fig(img, ind, title_=None, DR=None):
     plt.show()
 
 def envelope_detection(signal, DR=None):
-    # check rank
-    if len(signal.shape) == 4: 
-        signal = signal.reshape(signal.shape[1:])
-    assert len(signal.shape) == 3
-    
+    '''
+    Parameters
+    ----------
+    signal : float32, numpy array
+    DR : int, dynamic range. The default is None.
+    Returns
+    -------
+    envelop-detected signal in float32. Its shape is same as input shape.
+    '''
     # check dtype
     if not np.isreal(signal).all():
-        raise TypeError('signal must be an one-channel or two-channel real-valued array')
-        
-    # check last dimension
-    if signal.shape[-1] == 2:
-        envelope = np.sqrt(signal[:,:,0]**2 + signal[:,:,1]**2)
-    elif signal.shape[-1] == 1 and np.isreal(signal).all():
-        envelope = np.abs(Signal.hilbert(signal, axis=0)).reshape(signal.shape[:2])
+        raise ValueError('signal must be an one-channel or two-channel real-valued array')
+    # check rank
+    if len(signal.shape) == 3:
+        signal = np.expand_dims(signal, axis=0)
+    shape = signal.shape
+    assert len(shape) == 4
+    
+    if shape[-1] == 2:
+        envelope = np.sqrt(signal[:,:,:,0]**2 + signal[:,:,:,1]**2)
+    elif shape[-1] == 1:
+        envelope = np.abs(Signal.hilbert(signal, axis=1))
     else:
-        raise TypeError(f'Ivalid shape {signal.shape} for signal')
-        
+        raise ValueError('signal channels must be one or two')
+    envelope = envelope.reshape(shape[:-1])
+    ratio = np.max(envelope, axis=(1,2),keepdims=True)
+    envelope = envelope/ratio
     if DR is None:
         return envelope
     else:
-        dB_img = 20*np.log10(envelope/np.max(envelope)+1e-16) + DR
+        dB_img = 20*np.log10(envelope + 1e-16) + DR
         return dB_img
 
 def set_env():
@@ -82,6 +106,7 @@ def get_custom_object():
     return custom_object
 
 def get_default(complex_network=True):
+    batch = 8
     if complex_network:
         decimation = 2
         size = (3,6)
@@ -90,15 +115,12 @@ def get_default(complex_network=True):
         decimation = 1
         size = (6,6)
         loss = 'MSE'
-    return decimation, size, loss
+    return decimation, size, loss, batch
 
 def save_model(model, history, name):
-    now = datetime.now()
-    day_month_year = now.strftime("%d%m%Y")
-    sub_dir = name + day_month_year
-    saved_dir = os.path.join(DIR_SAVED, sub_dir)
-    model_name = name + '.h5'
-    model_figname = name + '.png'
+    saved_dir = os.path.join(DIR_SAVED, name)
+    model_name = name + '.h5'      # model
+    model_figname = name + 'arc.png'  # model architecture 
     if not os.path.exists(saved_dir):
         try:
             os.mkdir(saved_dir)
@@ -106,9 +128,10 @@ def save_model(model, history, name):
             os.makedirs(saved_dir)
     model_saved_path = os.path.join(saved_dir, model_name)
     model_figpath = os.path.join(saved_dir, model_figname)
+    # plot and save model architecture
     tf.keras.utils.plot_model(model, to_file=model_figpath, show_shapes=True, show_layer_names=True, dpi=900)
     model.save(model_saved_path)
-    history_name = os.path.join(saved_dir, name + 'history.txt')
+    history_name = os.path.join(saved_dir, name + 'history.txt') # loss value per epoch
     with open(history_name, 'w') as f:
         f.write(str(history))
     plt.figure()
@@ -122,7 +145,7 @@ def save_model(model, history, name):
         plt.savefig(os.path.join(saved_dir,name+'.png'))
         plt.show()
 
-def get_axis(img, ind):
+def _get_axis(img, ind):
     assert len(img.shape) ==3
     r, c = img.shape[:2]
     if (ind+1)%4 == 0:
@@ -132,14 +155,14 @@ def get_axis(img, ind):
     file_name = 'Data_' + str((ind+1)//4 + 1) + '_delay_' + str(level) + '.mat'
     file_path = os.path.join(DIR_SIMULATION, file_name)
     data = io.loadmat(file_path)
-    dx = data.get('dx') * (513//img.shape[1])
-    dz = data.get('dz') * (513//img.shape[0])
-    depth = data.get('depth')
+    dx = data.get('dx') * (513/img.shape[1])
+    dz = data.get('dz') * (513/img.shape[0])
+    depth = data.get('depth')/2
     x_axis = np.linspace(0,dx*c-dx,c) * 1e3 # [mm]
     z_axis = np.linspace(0,dz*r-dz,r) * 1e3 + depth * 1e3 # [mm]
     xmin, xmax = (np.min(x_axis), np.max(x_axis))
     zmin, zmax = (np.min(z_axis), np.max(z_axis))
-    return (xmin, xmax, zmin, zmax)
+    return (xmin, xmax, zmax, zmin)
     
 def inference(model, testset):
     time_collection = []
@@ -149,9 +172,10 @@ def inference(model, testset):
         s = time.time()
         _ = model.predict(testset)
         e = time.time()
-        time_collection.append((s-e)*1000)
-    return np.mean(time_collection)
-    
+        time_collection.append(e-s)
+    return np.mean(time_collection)/testset.shape[0]
+
+
             
     
 
