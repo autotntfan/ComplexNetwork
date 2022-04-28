cae75b9c78edb450c99dea0de2dda83eb968c6c5
autotntfan <jam310157@gmail.com>
2022-04-28 12:12:55 +0800

    MS_SSIM and SSIM_MSE, last layer activation,save name
    add the support for MS-SSIM and SSIM-MSE
    replace last layer activation tanh with LeakyReLU
    improve the model saved name
    adjust some parameter such as kernel size

--- model.py
+++ model.py
@@ -7,8 +7,8 @@ Created on Tue Mar 15 22:13:05 2022
 import tensorflow as tf
 import csv
 import os
-from complexnn.activation import cReLU, ctanh, zReLU, modReLU
-from complexnn.loss import ComplexRMS, ComplexMAE, ComplexMSE
+from complexnn.activation import cReLU, ctanh, zReLU, modReLU, AmplitudeMaxout
+from complexnn.loss import ComplexRMS, ComplexMAE, ComplexMSE, SSIM, MS_SSIM, SSIM_MSE
 from complexnn.conv_test import ComplexConv2D
 from complexnn.bn_test import ComplexBatchNormalization
 from tensorflow.keras.layers import Input, Conv2D, Conv2DTranspose, BatchNormalization, Dropout, Concatenate, LeakyReLU
@@ -70,12 +70,13 @@ class Model():
     def build_model(self):
         self.save_info()
         # determine activation function
-        if self.activations in {'cReLU', 'zReLU', 'modReLU', 'LeakyReLU'}:
+        if self.activations in {'cReLU', 'zReLU', 'modReLU', 'LeakyReLU', 'AMU'}:
             self.activations = {
                 'cReLU': cReLU,
                 'zReLU': zReLU,
                 'modReLU': modReLU,
                 'LeakyReLU': LeakyReLU,
+                'AMU'      : AmplitudeMaxout
                 }[self.activations]
         else:
             if isinstance(self.activations, str):
@@ -83,11 +84,14 @@ class Model():
 
         
         # determine loss function 
-        if self.losses in {'ComplexRMS', 'ComplexMAE', 'ComplexMSE', 'MSE'}:
+        if self.losses in {'ComplexRMS', 'ComplexMAE', 'ComplexMSE', 'SSIM', 'MSE','MS_SSIM' ,'SSIM_MSE'}:
             self.losses = {
                 'ComplexRMS': ComplexRMS,
                 'ComplexMAE': ComplexMAE,
                 'ComplexMSE': ComplexMSE,
+                'SSIM'      : SSIM,
+                'MS_SSIM'   : MS_SSIM,
+                'SSIM_MSE'  : SSIM_MSE,
                 'MSE'       : MeanSquaredError()
                 }[self.losses]
             
@@ -144,14 +148,14 @@ class Model():
             self.downsample(16*self.filters, self.size),  #(bs, 32, 32, 128*2)
             self.downsample(32*self.filters, self.size),  #(bs, 16, 16, 256*2)
             self.downsample(64*self.filters, self.size), #(bs, 8, 8, 512*2)
-            self.downsample(64*self.filters, self.size), #(bs, 8, 8, 512*2)
+            self.downsample(64*self.filters, self.size), #(bs, 4, 4, 512*2)
             ]
         up_stack = [
             self.upsample(64*self.filters, self.size), #(bs, 8, 8, 512*2)
-            self.upsample(32*self.filters, self.size), #(bs, 8, 8, 512*2)
-            self.upsample(16*self.filters, self.size),  #(bs, 16, 16, 256*2)
-            self.upsample(8*self.filters, self.size),  #(bs, 32, 32, 128*2)
-            self.upsample(4*self.filters, self.size),  #(bs, 64, 64, 64*2)
+            self.upsample(32*self.filters, self.size), #(bs, 16, 16, 512*2)
+            self.upsample(16*self.filters, self.size),  #(bs, 32, 32, 256*2)
+            self.upsample(8*self.filters, self.size),  #(bs, 64, 64, 128*2)
+            self.upsample(4*self.filters, self.size),  #(bs, 128, 128, 64*2)
             ]
         if self.complex_network:
             last = self.convFunc(2*self.filters, self.size, strides=2, padding='same', transposed=True) #(bs, 256, 256, 16*2)
@@ -174,47 +178,47 @@ class Model():
         x = self.activations()(x)
           
         for _ in range(3):
-            x = self.convFunc(2*self.filters, self.size, padding='same', use_bias=False)(x)
+            x = self.convFunc(2*self.filters, 3, padding='same', use_bias=False)(x)
             x = self.bnFunc()(x)
             x = self.activations()(x)
-        x = self.convFunc(1, self.size, padding='same', use_bias=False)(x)
+        x = self.convFunc(1, 3, padding='same', use_bias=False)(x)
         x = self.bnFunc()(x)
     
         
         if self.forward:
             x = self.activations()(x)
             x = Concatenate(axis=-1)((x, inputs_forward))
-            x = self.convFunc(1, self.size, padding='same', use_bias=False)(x)
+            x = self.convFunc(2, 3, padding='same', use_bias=False)(x)
             x = self.bnFunc()(x)
             x = tanh(x)
             return tf.keras.Model(inputs=[inputs, inputs_forward], outputs=x)
         else:    
-            x = tanh(x)
+            x = tf.keras.layers.LeakyReLU()(x)
             return tf.keras.Model(inputs=inputs, outputs=x)
 
     def sanitized(self):
         if self.complex_network:
             assert self.input_shape[-1] == 2
-            if self.losses not in {'ComplexRMS', 'ComplexMAE', 'ComplexMSE'}:
+            if self.losses not in {'ComplexRMS', 'ComplexMAE', 'ComplexMSE','SSIM','MS_SSIM','SSIM_MSE'}:
                 raise KeyError('Invalid complex-valued loss function')
-            if self.activations not in {'cReLU', 'zReLU', 'modReLU', 'LeakyReLU'}:
+            if self.activations not in {'cReLU', 'zReLU', 'modReLU', 'LeakyReLU', 'AMU'}:
                 raise  KeyError('Unsupported activation')
         else:
             assert self.input_shape[-1] == 1
-            if self.losses not in {'MSE'}:
-                raise KeyError('Invalid complex-valued loss function')
+            if self.losses not in {'MSE','SSIM', 'MS_SSIM', 'SSIM_MSE'}:
+                raise KeyError('Invalid real-valued loss function')
             if self.activations not in {'LeakyReLU'}:
                 raise  KeyError('Unsupported activation')
     
     def generate_name(self):
-        type_ = 'complex' if self.complex_network else 'real'
-        forward = 'foward' if self.forward else 'Notfoward'
-        epochs = str(self.epochs)
-        return f'{type_}model_{forward}_{epochs}_{self.losses}_{self.activations}_'
-    
-    def save_info(self):
         now = datetime.now()
         day_month_year = now.strftime("%d%m%Y")
+        type_ = 'complex' if self.complex_network else 'real'
+        forward = 'forward' if self.forward else 'Notforward'
+        epochs = str(self.epochs)
+        return f'{type_}model_{forward}_{epochs}_{self.losses}_{self.activations}_' + day_month_year
+    
+    def save_info(self):
         saved_var = {
             'input_shape':self.input_shape,
             'forward':self.forward,
@@ -228,9 +232,8 @@ class Model():
             'epochs':self.epochs,
             'activation':self.activations,
             'loss':self.losses,
-            'savingtime':day_month_year
             }
-        file_name = self.generate_name() + day_month_year
+        file_name = self.generate_name()
         saved_dir = os.path.join(r'./modelinfo', file_name)
         file_name = file_name + '_parameters.txt'
         if not os.path.exists(saved_dir):
