1aa8949b554a93a678cd92a36b9d82fbd1d06490
autotntfan <jam310157@gmail.com>
2022-05-02 01:01:17 +0800

    Update loss.py

--- complexnn_V2.3/loss.py
+++ complexnn_V2.3/loss.py
@@ -13,6 +13,8 @@ import scipy.signal as Signal
 from tensorflow.keras.utils import get_custom_objects
 from tensorflow.keras.losses import MeanSquaredError, MeanAbsoluteError
 
+PI = tf.constant(np.pi)
+
 def ComplexRMS(y_true, y_pred):
     y_true, y_pred = _precheck(y_true, y_pred)
     return tf.sqrt(tf.reduce_mean(_get_square_error(y_true, y_pred)))
@@ -173,6 +175,9 @@ def _normalization(signal):
         normalized_target = (real**2 + imag**2)**0.5
     ratio = tf.reduce_max(normalized_target,axis=(1,2,3),keepdims=True)
     return signal/ratio
+
+def _angle(x):
+    return tf.math.angle(tf.complex(get_realpart(x),get_imagpart(x)))
  
 def _envelope_detection(signal):
     '''
@@ -186,8 +191,7 @@ def _envelope_detection(signal):
         a normalized Tensor with only real-valued channel.
 
     '''
-    channel = tf.shape(signal)[-1]//2
-    envelope = (signal[:,:,:,:channel]**2 + signal[:,:,:,channel:]**2)**0.5
+    envelope = (get_realpart(signal)**2 + get_imagpart(signal)**2)**0.5
     return _normalization(envelope)
    
 def _SSIM_core(y_true, y_pred, func):
@@ -212,29 +216,39 @@ def _SSIM_core(y_true, y_pred, func):
         # envelope_pred = _envelope_detection(y_pred)
         # return 1 - tf.reduce_mean(func(envelope_pred,envelope_true,max_val=1,filter_size=7))
         y_pred = _normalization(y_pred)
-        real_pdt = get_realpart(y_pred)
-        imag_pdt = get_imagpart(y_pred)
-        real_true = get_realpart(y_true)
-        imag_true = get_imagpart(y_true)
-        imag_ssim = 1 - tf.reduce_mean(func(imag_pdt,imag_true,max_val=2,filter_size=7))
-        real_ssim = 1 - tf.reduce_mean(func(real_pdt,real_true,max_val=2,filter_size=7))
+        real_pdt = get_realpart(y_pred) + 1
+        imag_pdt = get_imagpart(y_pred) + 1
+        real_true = get_realpart(y_true) + 1
+        imag_true = get_imagpart(y_true) + 1
+        imag_ssim = 1 - tf.reduce_mean(func(imag_pdt,imag_true,max_val=2,filter_size=9))
+        real_ssim = 1 - tf.reduce_mean(func(real_pdt,real_true,max_val=2,filter_size=9))
         return (imag_ssim + real_ssim)/2
+        # pi = 2*tf.acos(0.)
+        # angle_ssim = 1 - tf.reduce_mean(func(_angle(y_pred),_angle(y_true),max_val=2*pi,filter_size=7))
+        # envelope_ssim = 1 - tf.reduce_mean(func(envelope_pred,envelope_true,max_val=1,filter_size=7))
+        # return (angle_ssim + envelope_ssim)/2
+        
         
 def SSIM(y_true,y_pred):
+    y_true, y_pred = _precheck(y_true, y_pred)
     return _SSIM_core(y_true, y_pred, tf.image.ssim)
 
 def MS_SSIM(y_true,y_pred):
+    y_true, y_pred = _precheck(y_true, y_pred)
     return _SSIM_core(y_true, y_pred, tf.image.ssim_multiscale)
     
 def SSIM_MSE(y_true,y_pred):
+    y_true, y_pred = _precheck(y_true, y_pred)
     ratio = 0.2
     if y_pred.shape[-1]%2:
         mse = MeanSquaredError()
         mse = mse(y_true, y_pred)
     else:
-        mse = ComplexMSE(y_true, y_pred)
+        mse = MeanSquaredError()
+        mse = mse(PI + _angle(y_true),PI + _angle(y_pred))
+        # mse = ComplexMSE(y_true, y_pred)
     ssim = SSIM(y_true,y_pred)
-    return ratio*ssim + mse
+    return ssim + mse
     # return ssim*mse
     
 get_custom_objects().update({'SSIM': SSIM,
