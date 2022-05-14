f5ce3e56f008ea8e145ad4ae922e5d73739d0731
autotntfan <jam310157@gmail.com>
2022-05-14 16:48:10 +0800

    Update train.py
    support for custom training

--- train.py
+++ train.py
@@ -4,47 +4,49 @@ Created on Wed Mar 16 21:24:37 2022
 
 @author: benzener
 """
-
+import tensorflow as tf
+try:
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(gpus[0], True)
+except RuntimeError as e:
+    print(e)
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 from train_utils import show_fig, envelope_detection, inference, phase_diff, projection
-from train_utils import set_env, get_custom_object, get_default
+from train_utils import get_custom_object, get_default
 from train_utils import save_model, save_metrics
 from newdataset import DataPreprocessing, GetData
 from model import Model
 from DisplayedImg import VerifyPred, Fig, Difference, BasedCompute
 import tensorflow as tf
 import numpy as np
-import os
+
 '''
     This file is main script running model and plotting outputs.
 '''
-# check and allow gpu memory to grow
-try:
-    set_env()
-except RuntimeError as e:
-    print(e)
 
-LOAD_MODEL         = True   # whether load pretrained model, True or False
+LOAD_MODEL         = False   # whether load pretrained model, True or False
 FORWARD            = False   # whether use forward UNet or not, True or False
 CALLBACK           = False   # whether add callbacks in model fit, True or False
 COMPLEX            = True  # whether use complex- or real-valued network, True or False
 USING_DEFAULT      = False
-FOCUS              = True
+
 
 NUM_TRAINING       = 1800    # number of training data, max:1600
-DECIMATION         = 2       # downsample factor
+DECIMATION         = 2      # downsample factor
 FILTERS            = 16      # number of filters for the shallowest conv2D layer
 SIZE               = (3,3)    # size of each Conv2D kernel
 BATCH_SIZE         = 8       # mini-batch size
 LR                 = 5*1e-4    # learning rate of optimizer
-EPOCHS             = 2     # training epochs
-VALIDATION_SPLIT   = 0.2     # ratio of validation data referred to training data. ratio = # of val_data/ # of training_data
+EPOCHS             = 200     # training epochs
+VALIDATION_SPLIT   = 0     # ratio of validation data referred to training data. ratio = # of val_data/ # of training_data
 NFIG               = 93       # show the n-th fig of speckle, target, or prediction
 DR                 = 60      # dynamic range in dB
 
 ACTIVATION         = 'FLeakyReLU'  # Hidden-layer activation function, it could be 'modeReLU', 'cReLU', ... etc
 LOSS               = 'SSIM'  # loss function,it could be 'cMSE', 'cMAE', 'cRMS'
 DIR_SAVED          = r'./modelinfo'
-DIR_SIMULATION     = r'./simulation_straight'
+DIR_SIMULATION     = r'./simulation_data'
 
 if USING_DEFAULT:
     DECIMATION, SIZE, LOSS, BATCH_SIZE = get_default(COMPLEX)
@@ -86,6 +88,7 @@ if LOAD_MODEL:
             # model_name = 'complexmodel_Notforward_300_SSIM_LeakyReLU_01052022_filter15new'
             # model_name = 'complexmodel_Notforward_300_SSIM_LeakyReLU_01052022'
             # model_name = 'complexmodel_Notforward_300_SSIM_FLeakyReLU_04052022'
+            # model_name = 'complexmodel_Notforward_200_SSIM_LeakyReLU_08052022'
             model_name = 'complexmodel_Notforward_198_SSIM_FLeakyReLU_05052022'
         else:
             # model_name = 'realmodel_Notforward_200_MSE_LeakyReLU_30032022'
@@ -111,6 +114,7 @@ else:
                  lr=LR,
                  epochs=EPOCHS,
                  validation_split=VALIDATION_SPLIT,
+                 validation_data=None,
                  activations=ACTIVATION,
                  losses=LOSS,
                  callbacks=callbacks,
@@ -121,17 +125,19 @@ else:
         prediction = model.predict([x_test, ideal_test])
     else:
         model, history = UNet(x_train, y_train)
+        # model, history = UNet.running(x_train, y_train)
         prediction = model.predict(x_test)
-    save_model(model, history.history, model_name)
-      
+        save_model(model, history.history, model_name)
+        # save_model(model, history, model_name)
 level, ind =  get_dataset.find_level(NFIG, train=False)
 V = VerifyPred(prediction[NFIG],
                y_test[NFIG],
-               ind, 
+               ind,
                DR, 
                model_name=model_name,
                DIR_SAVED=DIR_SAVED,
                DIR_SIMULATION=DIR_SIMULATION)
+Fig().envelope_fig(x_test[NFIG], DR, model_name, ind, 'speckle' + str(level))
 V.pred_img
 V.truth_img
 V.show_lateral_projection
@@ -139,8 +145,6 @@ V.show_axial_projection
 V.show_complex_diff()
 V.show_phase_diff()
 Fig().error_boxplot(prediction, y_test, get_dataset, model_name)
-
-show_fig(x_test[NFIG], ind, 'speckle' + str(level), DR, model_name)
 # show_fig(prediction[NFIG], ind, 'prediction' + str(level), DR, model_name)
 # show_fig(y_test[NFIG], ind, 'psf' + str(level), DR, model_name)
 
