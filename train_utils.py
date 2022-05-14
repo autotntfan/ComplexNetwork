30fc4ed82267b5aa7893fd461827e6e64221dfe5
autotntfan <jam310157@gmail.com>
2022-05-14 16:49:28 +0800

    Update train_utils.py
    Remove comment
    It will combine with DisplayedImg
    Revise history plotting if validation doesn't exist

--- train_utils.py
+++ train_utils.py
@@ -174,7 +174,9 @@ def save_model(model, history, name):
     plt.figure()
     plt.plot(history['loss'], label='training loss')
     try:
-        plt.plot(history['val_loss'], label='val loss')
+        plt.plot(history['val_loss'], label='val loss')    
+    except KeyError:
+        print('No validation')
     finally:
         plt.legend()
         plt.xlabel('epochs')
@@ -207,7 +209,6 @@ def _get_axis(img, ind, fs=False):
     else:
         level = (ind+1)%4
     file_name = 'Data_' + str(ind//4 + 1) + '_delay_' + str(level) + '.mat'
-    print(file_name)
     file_path = os.path.join(DIR_SIMULATION, file_name)
     data = io.loadmat(file_path)
     dx = data.get('dx') * (DATA_SIZE[2]/img.shape[1])
@@ -397,152 +398,3 @@ def multimodel(test_img):
         show_fig(prediction,DR=60)
         del model
     
-        
-# class PostProcessing():
-    
-#     def __init__(self,
-#                  model,
-#                  name=None,
-#                  DR=None,
-#                  DIR_SAVED=r'./modelinfo',
-#                  DIR_SIMULATION=r'./simulation_straight'):
-#         self.model = model
-#         self.name = name
-#         self.DR = DR
-        
-#         self.DIR_SAVED = DIR_SAVED
-#         self.DIR_SIMULATION = DIR_SIMULATION
-        
-#     def show_fig(self, img, ind=None, title_=None, saved=False):
-        
-#         # check image dimension is 3 or 4
-#         if len(img.shape) == 4:
-#             if img.shape[0] != 1:
-#                 raise ValueError('unable to support multiple images')
-#             shape = img.shape[1:-1]
-#         elif len(img.shape) == 3:
-#             shape = img.shape[:-1]
-#         else:
-#             raise ValueError('invalid image shape')
-#         if ind is None:
-#             axis = None
-#         else:
-#             axis = self._get_axis(img, ind)
-#         plt.figure(dpi=300)
-#         envelope_img = self.envelope_detection(img, self.DR)
-#         envelope_img = envelope_img.reshape(shape)
-#         assert len(envelope_img.shape) == 2
-#         if self.DR is not None:
-#             plt.imshow(envelope_img,
-#                        cmap='gray',
-#                        vmin=0,vmax=self.DR,
-#                        extent=axis,
-#                        aspect='auto')
-#             plt.colorbar()
-#         else:
-#             plt.imshow(envelope_img, cmap='gray',extent=axis,aspect='auto')
-#         if title_ is not None:
-#             assert isinstance(title_, str)
-#             plt.title(title_)
-#         plt.xlabel('Lateral position (mm)')
-#         plt.ylabel('Depth (mm)')
-#         if saved:
-#             saved_name = os.path.join(self.DIR_SAVED, self.name , title_ + '.png')
-#             plt.savefig(saved_name, dpi=300)
-#         plt.show()
-      
-#     def envelope_detection(self, signal):
-        
-#         '''
-#         Parameters
-#         ----------
-#         signal : float32, numpy array
-#         DR : int, dynamic range. The default is None.
-#         Returns
-#         -------
-#         envelop-detected signal in float32. Its shape is same as input shape.
-#         '''
-#         # check dtype
-#         if not np.isreal(signal).all():
-#             raise ValueError('signal must be an one-channel or two-channel real-valued array')
-#         # check rank
-#         if len(signal.shape) == 3:
-#             signal = np.expand_dims(signal, axis=0)
-#         shape = signal.shape
-#         assert len(shape) == 4
-        
-#         if shape[-1] == 2:
-#             envelope = np.sqrt(signal[:,:,:,0]**2 + signal[:,:,:,1]**2)
-#         elif shape[-1] == 1:
-#             envelope = np.abs(Signal.hilbert(signal, axis=1))
-#         else:
-#             raise ValueError('signal channels must be one or two')
-#         envelope = envelope.reshape(shape[:-1])
-#         ratio = np.max(envelope, axis=(1,2),keepdims=True)
-#         envelope = envelope/ratio
-#         if self.DR is None:
-#             return envelope
-#         else:
-#             dB_img = 20*np.log10(envelope + 1e-16) + self.DR
-#             return dB_img
-          
-#     def save_model(self, history):
-        
-#         saved_dir = os.path.join(self.DIR_SAVED, self.name)
-#         model_name = self.name + '.h5'      # model
-#         model_arcname = self.name + 'arc.png'  # model architecture
-#         if not os.path.exists(saved_dir):
-#             try:
-#                 os.mkdir(saved_dir)
-#             except FileNotFoundError:
-#                 os.makedirs(saved_dir)
-#         model_saved_path = os.path.join(saved_dir, model_name)
-#         model_arcpath = os.path.join(saved_dir, model_arcname)
-#         # plot and save model architecture
-#         tf.keras.utils.plot_model(self.model, to_file=model_arcpath, show_shapes=True, show_layer_names=True, dpi=900)
-#         self.model.save(model_saved_path)
-#         history_name = os.path.join(saved_dir, self.name + 'history.txt') # loss value per epoch
-#         with open(history_name, 'w') as f:
-#             f.write(str(history))
-#         plt.figure()
-#         plt.plot(history['loss'], label='training loss')
-#         try:
-#             plt.plot(history['val_loss'], label='val loss')
-#         finally:
-#             plt.legend()
-#             plt.xlabel('epochs')
-#             plt.ylabel('loss')
-#             plt.savefig(os.path.join(saved_dir,self.name+'.png'))
-#             plt.show()
-            
-#     def inference(self, testset):
-        
-#         time_collection = []
-#         for i in range(10):
-#             _ = self.model.predict(testset[i:i+10])
-#         for i in range(10):
-#             s = time.time()
-#             _ = self.model.predict(testset)
-#             e = time.time()
-#             time_collection.append(e-s)
-#         return np.mean(time_collection)/testset.shape[0]
-    
-#     def _get_axis(self, img, ind):
-        
-#         assert len(img.shape) ==3
-#         r, c = img.shape[:2]
-#         if (ind+1)%4 == 0:
-#             level = 4
-#         else:
-#             level = (ind+1)%4
-#         file_name = 'Data_' + str((ind+1)//4 + 1) + '_delay_' + str(level) + '.mat'
-#         file_path = os.path.join(self.DIR_SIMULATION, file_name)
-#         data = io.loadmat(file_path)
-#         dx = data.get('dx') * (513/img.shape[1])
-#         dz = data.get('dz') * (513/img.shape[0])
-#         depth = data.get('depth')/2
-#         x_axis = np.linspace(0,dx*c-dx,c) * 1e3 # [mm]
-#         z_axis = np.linspace(0,dz*r-dz,r) * 1e3 + depth * 1e3 # [mm]
-#         xmin, xmax = (np.min(x_axis), np.max(x_axis))
-#         zmin, zmax = (np.min(z_axis), np.max(z_axis))
-#         return (xmin, xmax, zmax, zmin)
