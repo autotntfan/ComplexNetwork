95542cc632f281d8eb4a103f8d0f28e960d86249
autotntfan <jam310157@gmail.com>
2022-07-12 13:00:37 +0800

    Update train.py
    new way for measure metrics,
    save parameters and read it, precheck loaded file is valid

--- train.py
+++ train.py
@@ -12,14 +12,17 @@ except RuntimeError as e:
     print(e)
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
-from train_utils import show_fig, envelope_detection, inference, phase_diff, projection
 from train_utils import get_custom_object, get_default
 from train_utils import save_model, save_metrics
 from newdataset import DataPreprocessing, GetData
 from model import Model
 from DisplayedImg import VerifyPred, Fig
+from scipy import io
+from computation import BasedCompute, Difference
+from metrics import Metrics
 import tensorflow as tf
 import numpy as np
+
 
 '''
     This file is main script running model and plotting outputs.
@@ -42,9 +45,10 @@ EPOCHS             = 200     # training epochs
 VALIDATION_SPLIT   = 0.2     # ratio of validation data referred to training data. ratio = # of val_data/ # of training_data
 NFIG               = 93       # show the n-th fig of speckle, target, or prediction
 DR                 = 60      # dynamic range in dB
+SEED               = 7414
 
 ACTIVATION         = 'FLeakyReLU'  # Hidden-layer activation function, it could be 'modeReLU', 'cReLU', ... etc
-LOSS               = 'SSIM_MSE'  # loss function,it could be 'cMSE', 'cMAE', 'cRMS'
+LOSS               = 'SSIM'  # loss function,it could be 'cMSE', 'cMAE', 'cRMS'
 DIR_SAVED          = r'./modelinfo'
 DIR_SIMULATION     = r'./simulation_data'
 
@@ -58,7 +62,12 @@ if NUM_TRAINING != 1800:
 get_dataset = GetData(factor=DECIMATION,
                       num_training=NUM_TRAINING,
                       complex_network=COMPLEX,
+                      seed=SEED,
                       forward=FORWARD)
+data_info = get_dataset.info()
+if data_info['seed'] != SEED:
+    raise ValueError('Dataset is not compatible')
+B = BasedCompute()
 if FORWARD:
     (x_train, y_train), (x_test, y_test), (ideal_train, ideal_test) = get_dataset()
 else:
@@ -93,16 +102,23 @@ if LOAD_MODEL:
             # model_name = 'complexmodel_Notforward_200_MS_SSIM_LeakyReLU_09052022'
             # model_name = 'complexmodel_Notforward_198_SSIM_FLeakyReLU_11052022'
             # model_name = 'complexmodel_Notforward_200_SSIM_FLeakyReLU_20052022'
+            # model_name = 'complexmodel_Notforward_200_SSIM_FLeakyReLU_14052022'
             # model_name = 'complexmodel_Notforward_200_SSIM_LeakyReLU_21052022'
             # model_name = 'complexmodel_Notforward_200_ComplexMSE_LeakyReLU_22052022'
             # model_name = 'complexmodel_Notforward_200_ComplexMSE_FLeakyReLU_22052022'
             # model_name = 'complexmodel_Notforward_200_SSIM_FLeakyReLU_14052022'
-            model_name = 'complexmodel_Notforward_200_SSIM_MSE_FLeakyReLU_25052022'
+            # model_name = 'complexmodel_Notforward_200_SSIM_MSE_FLeakyReLU_25052022'
+            model_name = 'complexmodel_Notforward_200_SSIM_FLeakyReLU_30052022'
+            # model_name = 'complexmodel_Notforward_200_SSIM_FLeakyReLU_13062022'
+            # model_name = 'complexmodel_Notforward_198_SSIM_MSE_FLeakyReLU_24052022'
         else:
             # model_name = 'realmodel_Notforward_200_MSE_LeakyReLU_30032022'
             # model_name = 'realmodel_Notforward_200_SSIM_LeakyReLU_19042022'
             # model_name = 'realmodel_Notforward_100_SSIM_MSE_LeakyReLU_20042022'
             model_name = 'realmodel_Notforward_300_SSIM_LeakyReLU_26042022'
+        # info = B.read_info(model_name)
+        # if info['seed'] != SEED or info['validation_split'] != VALIDATION_SPLIT:
+        #     raise ValueError('Dataset is not compatible')
         model = tf.keras.models.load_model(os.path.join(DIR_SAVED,model_name,model_name+'.h5'),
                                            custom_objects=custom_object)
         prediction = model.predict(x_test)
@@ -128,6 +144,7 @@ else:
                  callbacks=callbacks,
                  complex_network=COMPLEX)
     model_name = UNet.generate_name()
+
     if FORWARD:
         model, history = UNet([x_train, ideal_train], y_train)
         prediction = model.predict([x_test, ideal_test])
@@ -135,13 +152,15 @@ else:
         model, history = UNet(x_train, y_train)
         # model, history = UNet.running(x_train, y_train)
         prediction = model.predict(x_test)
-        save_model(model, history.history, model_name)
+    data_info.update(UNet.info())
+    save_model(model, history.history, model_name)
         # save_model(model, history, model_name)
+    B.save_info(model_name, data_info)
 level, ind =  get_dataset.find_level(NFIG, train=False)
 V = VerifyPred(prediction[NFIG],
                y_test[NFIG],
                ind,
-               DR, 
+               DR,
                model_name=model_name,
                DIR_SAVED=DIR_SAVED,
                DIR_SIMULATION=DIR_SIMULATION)
@@ -153,42 +172,11 @@ V.show_axial_projection
 V.show_complex_diff()
 V.show_phase_diff()
 Fig().err_fig(prediction, y_test, get_dataset, model_name)
-Fig().topn_err_fig(prediction, y_test, get_dataset, 3, model_name)
+Fig().topn_err_fig(prediction, y_test, get_dataset, 5, model_name)
 # show_fig(prediction[NFIG], ind, 'prediction' + str(level), DR, model_name)
 # show_fig(y_test[NFIG], ind, 'psf' + str(level), DR, model_name)
 
 # metrics
-mse = tf.keras.losses.MeanSquaredError()
-
-shape = (1,) + prediction[NFIG].shape[:-1] + (1,)
-envelope_pred = envelope_detection(prediction[NFIG]).reshape(shape)
-envelope_true = envelope_detection(y_test[NFIG]).reshape(shape)
-    
-print("MSE", mse(envelope_pred,envelope_true).numpy())
-print("SSIM", tf.image.ssim(envelope_pred,
-                            envelope_true,
-                            max_val=1,
-                            filter_size=7).numpy())
-print("MS-SSIM", tf.image.ssim_multiscale(envelope_pred,
-                                        envelope_true,
-                                        max_val=1,
-                                        filter_size=7).numpy())
-# print('inference time', inference(model, x_test))
-
-envelope_pred = np.expand_dims(envelope_detection(prediction),axis=-1)
-envelope_true = np.expand_dims(envelope_detection(y_test),axis=-1)
-print('average MSE',tf.reduce_mean(mse(envelope_pred,envelope_true)).numpy())
-print("average SSIM", tf.reduce_mean(tf.image.ssim(envelope_pred,
-                                                   envelope_true,
-                                                   max_val=1,
-                                                   filter_size=7)).numpy())
-print("average MS-SSIM", tf.reduce_mean(tf.image.ssim_multiscale(envelope_pred,
-                                                                 envelope_true,
-                                                                 max_val=1,
-                                                                 filter_size=7)).numpy())
-
-# phase_diff(y_test[NFIG], prediction[NFIG], 0.5, model_name)
-save_metrics(envelope_pred, envelope_true, model_name)
-# projection(prediction[NFIG], y_test[NFIG], model_name)
-# projection(prediction[NFIG], y_test[NFIG], model_name, direction='axial')
+M = Metrics()
+M.save_info(prediction, y_test, model_name)
 tf.keras.backend.clear_session()
