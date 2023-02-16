86b9779cd2559ad6698c9d5733f22563926446ac
benzener <jam310157@gmail.com>
2023-02-16 19:36:21 +0800

    Add coefficients

--- baseband/setting/constant.py
+++ baseband/setting/constant.py
@@ -10,10 +10,13 @@ MODELPATH = r'./modelinfo' # path of saved model
 DATAPATH  = r'./simulation_data' # path of simulation data
 CACHEPATH = r'./parameters' # path of splitted data
 
-DATASIZE  = (2000,257,257) # simulation data size
-EPS       = 1e-16
-SOUNDV    = 1540
-NELEMENT  = 128
+DATASIZE    = (2000,257,257) # simulation data size
+FOV         = (16,32)        # field of view, unit in lambda, i.e. (H,W) = (16lambda,32lambda)
+EPS         = 1e-16
+SOUNDV      = 1540
+NELEMENT    = 128
+BEAMSPACING = 4
+FIRORDER    = 48
 
 if not os.path.exists(MODELPATH):
     os.mkdir(MODELPATH)
